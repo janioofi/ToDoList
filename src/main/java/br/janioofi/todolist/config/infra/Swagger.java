c61a2f21d50d2610268b4a242021acaaa7709756
Jânio <janioofi@gmail.com>
2024-07-26 20:26:17 -0300

    Configuração do swagger

---
+++ src/main/java/br/janioofi/todolist/config/infra/Swagger.java
@@ -0,0 +1,36 @@
+package br.janioofi.todolist.config.infra;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Swagger {
+
+    @Bean
+    public GroupedOpenApi publicApi(){
+        return GroupedOpenApi.builder()
+                .group("To Do List")
+                .displayName("API")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("To Do List - API")
+                        .description("Rest API")
+                        .contact(new Contact()
+                                .name("@janioofi")
+                                .email("janioofi@gmail.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.linkedin.com/in/janioofi/")));
+    }
+}

8ca04237795e3f07c8477efd646e1c3ff083dfc8
Jânio <janioofi@gmail.com>
2024-07-26 20:25:49 -0300

    Criação das exceptions

---
+++ src/main/java/br/janioofi/todolist/controllers/ApplicationControllerAdvice.java
@@ -0,0 +1,50 @@
+package br.janioofi.todolist.controllers;
+
+import br.janioofi.todolist.domain.exceptions.ResourceNotFoundException;
+import br.janioofi.todolist.domain.exceptions.ValidationErrors;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFoundException(ResourceNotFoundException ex){
+        log.error(ex.getMessage());
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrors validationErrors(ConstraintViolationException ex, HttpServletRequest request){
+        ValidationErrors field = new ValidationErrors();
+        field.setPath(request.getRequestURI());
+        field.setStatus(HttpStatus.BAD_REQUEST.value());
+        field.setError("Validation Error");
+        for(var x : ex.getConstraintViolations()){
+            field.addErrors(x.getPropertyPath().toString(), x.getMessage());
+        }
+        log.error(field.toString());
+        return field;
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrors validationErrors(DataIntegrityViolationException ex, HttpServletRequest request){
+        ValidationErrors field = new ValidationErrors();
+        field.setPath(request.getRequestURI());
+        field.setStatus(HttpStatus.BAD_REQUEST.value());
+        field.setError("Validation Error");
+        field.addErrors("Data Integrity Violation", ex.getMessage());
+        log.error(field.toString());
+        return field;
+    }
+}

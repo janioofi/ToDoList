390c4b64f035d598e8b4f094038785520c844a95
Jânio <janioofi@gmail.com>
2024-07-26 23:41:43 -0300

    Service para usuario

--- src/main/java/br/janioofi/todolist/domain/entities/Usuario.java
+++ src/main/java/br/janioofi/todolist/domain/entities/Usuario.java
@@ -1,14 +1,18 @@
 package br.janioofi.todolist.domain.entities;
 
-
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
@@ -16,22 +20,61 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "tb_usuario")
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idUsuario;
 
-    @NotBlank
     @NotNull
     @Column(unique = true)
     private String usuario;
 
-    @NotBlank
     @NotNull
     private String senha;
 
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Tarefa> tarefas;
+
+    public Usuario(String usuario, String senha) {
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return getSenha();
+    }
+
+    @Override
+    public String getUsername() {
+        return getUsuario();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
--- src/main/java/br/janioofi/todolist/domain/services/UsuarioService.java
+++ src/main/java/br/janioofi/todolist/domain/services/UsuarioService.java
@@ -1,4 +1,73 @@
 package br.janioofi.todolist.domain.services;
 
+
+import br.janioofi.todolist.domain.dtos.Mapper;
+import br.janioofi.todolist.domain.dtos.UsuarioRequestDto;
+import br.janioofi.todolist.domain.dtos.UsuarioResponseDto;
+import br.janioofi.todolist.domain.entities.Usuario;
+import br.janioofi.todolist.domain.exceptions.ResourceNotFoundException;
+import br.janioofi.todolist.domain.repositories.UsuarioRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class UsuarioService {
+
+    private final UsuarioRepository repository;
+    private static final String NENHUM_USUARIO = "Nenhum usuário encontrado com ID: ";
+    private static final String USARIO_ERRADO = "Apenas o próprio usuário pode executar está ação";
+
+    public List<UsuarioResponseDto> findAll(){
+        log.info("Listando todos usuários");
+        return repository.findAll().stream().map(Mapper::toDto).toList();
+    }
+
+    public UsuarioResponseDto findById(Long id){
+        log.info("Buscando usuário com ID: {}", id);
+        return repository.findById(id).map(Mapper::toDto).orElseThrow(() -> new ResourceNotFoundException(NENHUM_USUARIO + id));
+    }
+
+    public void delete(Long id, HttpServletResponse response){
+        Optional<Usuario> usuario = repository.findById(id);
+        if(usuario.isEmpty()) throw new ResourceNotFoundException(NENHUM_USUARIO + id);
+        validaUsuario(response, id);
+        log.info("Deletando usuário com ID: {}", id);
+        repository.deleteById(id);
+    }
+
+    public UsuarioResponseDto update(@Valid Long id, UsuarioRequestDto user, HttpServletResponse response){
+        validaCriacaoEdicao(user, id);
+        validaUsuario(response, id);
+        String encryptedPassword = new BCryptPasswordEncoder().encode(user.senha());
+        Usuario usuario = repository.findById(id).map(recordFound -> {
+            recordFound.setUsuario(user.usuario());
+            recordFound.setSenha(encryptedPassword);
+            return repository.save(recordFound);
+        }).orElseThrow(() -> new ResourceNotFoundException(NENHUM_USUARIO + id));
+        log.info("Realizando uma atualização pro usuário com ID: {}", id);
+        return Mapper.toDto(usuario);
+    }
+
+    private void validaCriacaoEdicao(UsuarioRequestDto objDTO, Long id) {
+        Usuario obj = repository.findByUsuario(objDTO.usuario());
+        if (obj != null && !obj.getIdUsuario().equals(id)) {
+            throw new DataIntegrityViolationException("Usuário já existe no sistema!");
+        }
+    }
+
+    private void validaUsuario(HttpServletResponse response, Long id) {
+        Usuario usuarioHeader = repository.findByUsuario(response.getHeader("User"));
+        Usuario usuario = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(NENHUM_USUARIO + id));
+        if(!usuario.getIdUsuario().equals(usuarioHeader.getIdUsuario())) throw new ResourceNotFoundException(USARIO_ERRADO);
+    }
 }

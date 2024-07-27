0361187e0430455b7381b1f64ac122e672c01964
Jânio <janioofi@gmail.com>
2024-07-26 23:43:14 -0300

    Nova exceção

--- src/main/java/br/janioofi/todolist/controllers/ApplicationControllerAdvice.java
+++ src/main/java/br/janioofi/todolist/controllers/ApplicationControllerAdvice.java
@@ -7,6 +7,7 @@ import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -39,6 +40,18 @@ public class ApplicationControllerAdvice {
     @ExceptionHandler(DataIntegrityViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ValidationErrors validationErrors(DataIntegrityViolationException ex, HttpServletRequest request){
+        ValidationErrors field = new ValidationErrors();
+        field.setPath(request.getRequestURI());
+        field.setStatus(HttpStatus.BAD_REQUEST.value());
+        field.setError("Validation Error");
+        field.addErrors("Data Integrity Violation", ex.getMessage());
+        log.error(field.toString());
+        return field;
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrors validationErrors(HttpMessageNotReadableException ex, HttpServletRequest request){
         ValidationErrors field = new ValidationErrors();
         field.setPath(request.getRequestURI());
         field.setStatus(HttpStatus.BAD_REQUEST.value());

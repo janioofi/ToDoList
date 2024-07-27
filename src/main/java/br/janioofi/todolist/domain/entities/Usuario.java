13394efdde3dddd8349aaca0572401da23b7795f
Jânio <janioofi@gmail.com>
2024-07-27 01:01:32 -0300

    configuração do sql

--- src/main/java/br/janioofi/todolist/domain/entities/Usuario.java
+++ src/main/java/br/janioofi/todolist/domain/entities/Usuario.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
 
 @Getter
